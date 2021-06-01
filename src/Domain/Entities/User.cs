2696c563d26c8535d06176b41a700f465f6e97b4
thetomek <tommac.opole@gmail.com>
2021-06-01 21:02:30 +0200

    Added enums for entities

---
+++ src/Domain/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public UserRole Role { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public ICollection<Offer> SoldOffers { get; set; }
+        public ICollection<Offer> Acquisitions { get; set; }
+
+        public bool IsActive { get; set; }
+
+    }
+}
