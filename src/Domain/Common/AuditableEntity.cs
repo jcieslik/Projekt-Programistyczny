104a406e2bfcd570dd485e5b1436bc0a22b54ef0
thetomek <tommac.opole@gmail.com>
2021-06-01 21:02:08 +0200

    Added Auditable Entity class

---
+++ src/Domain/Common/AuditableEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Domain.Common
+{
+    public abstract class AuditableEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime Created { get; set; }
+        public Guid CreatedBy { get; set; }
+        public DateTime? LastModified { get; set; }
+        public Guid? ModifiedBy { get; set; }
+    }
+}
