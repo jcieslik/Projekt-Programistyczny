cc5affe560a1c32932a7416a1caad55fbad24a11
Jakub Cieślik <jakucie936@student.polsl.pl>
2021-06-02 18:48:22 +0200

    fix

--- src/Projekt-Programistyczny/Startup.cs
+++ src/Projekt-Programistyczny/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Projekt_Programistyczny.Extensions;
 using Projekt_Programistyczny.Services;
 using System;
 using System.Collections.Generic;
