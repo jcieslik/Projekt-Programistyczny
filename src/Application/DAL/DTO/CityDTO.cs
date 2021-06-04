77b43005e0e5256d902037e6cba41f15e023c8a8
thetomek <tommac.opole@gmail.com>
2021-06-04 18:08:59 +0200

    Created some Dtos, filter model, BaseDataService and Filter Service

---
+++ src/Application/DAL/DTO/CityDTO.cs
@@ -0,0 +1,11 @@
+﻿using Application.Common.Dto;
+using Application.Common.Mappings;
+using Domain.Entities;
+
+namespace Application.DAL.DTO
+{
+    public class CityDTO : EntityDTO, IMapFrom<City>
+    {
+        public string Name { get; set; }
+    }
+}
