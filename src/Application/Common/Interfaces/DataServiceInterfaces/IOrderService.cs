f8b802bee2d3ac9283c586edfdf49d1cdee0182b
thetomek <tommac.opole@gmail.com>
2021-06-15 19:11:47 +0200

    Order service

---
+++ src/Application/Common/Interfaces/DataServiceInterfaces/IOrderService.cs
@@ -0,0 +1,18 @@
+﻿using Application.DAL.DTO;
+using Application.DAL.DTO.CommandDTOs.Create;
+using Application.DAL.DTO.CommandDTOs.Update;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Application.Common.Interfaces.DataServiceInterfaces
+{
+    public interface IOrderService
+    {
+        Task<OrderDTO> CreateOrderAsync(CreateOrderDTO dto);
+        Task<OrderDTO> CreateOrderAsync(UpdateOrderDTO dto);
+        Task<OrderDTO> GetOrderByIdAsync(Guid id);
+        Task<IEnumerable<OrderDTO>> GetOrdersFromOffer(Guid offerId);
+        Task<IEnumerable<OrderDTO>> GetOrdersFromUser(Guid userId);
+    }
+}
