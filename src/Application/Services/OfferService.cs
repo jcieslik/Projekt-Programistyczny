6c2e8dd823d9061b5e3bcd68196f3cb30b468b36
Po Tato <meammeu@gmail.com>
2021-06-27 22:56:30 +0200

    Fixed CategoryId being optional

--- src/Application/Services/OfferService.cs
+++ src/Application/Services/OfferService.cs
@@ -99,7 +99,7 @@ namespace Application.Services
                 .AsNoTracking()
                 .Where(x => !x.IsHidden && x.State == OfferState.Awaiting);
 
-            if (filterModel.CategoryId.HasValue)
+            if (filterModel.CategoryId > 0)
             {
                 List<long> ids = GetChildrenCategoriesIds(filterModel.CategoryId.Value);
                 ids.Add(filterModel.CategoryId.Value);
