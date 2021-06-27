01a7ce707e9d99fc6a830a055ce079d9159c6623
Jakub Cieślik <jakucie936@student.polsl.pl>
2021-06-27 17:37:09 +0200

    payment using stripe

---
+++ src/Application/DAL/DTO/PaymentRequestDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DAL.DTO
+{
+    public class PaymentRequestDTO
+    {
+        public string tokenId { get; set; }
+        public int amount { get; set; }
+    }
+}
---
+++ src/Projekt-Programistyczny/Controllers/PaymentController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Stripe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.DAL.DTO;
+
+namespace Projekt_Programistyczny.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        public PaymentController() {}
+
+        [HttpPost]
+        [Route("MakePayment")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> MakePayment([FromBody] PaymentRequestDTO paymentRequest)
+        {
+            StripeConfiguration.ApiKey = "sk_test_51J6t0pFBQwYWjNW6dpz7HqgexrbBGzvpDT9yeWARx0KUJNhiTX76XF8WZ9pGcamTDuZl4NkSuzr3Ms0QEkE7e1tc00s6Wr0tZd";
+
+            var options = new ChargeCreateOptions
+            {
+                Amount = paymentRequest.amount,
+                Currency = "pln",
+                Source = paymentRequest.tokenId,
+                Description = "My First Test Charge (created for API docs)",
+            };
+            var service = new ChargeService();
+            service.Create(options);
+
+            return Ok();
+        }
+    }
+}
