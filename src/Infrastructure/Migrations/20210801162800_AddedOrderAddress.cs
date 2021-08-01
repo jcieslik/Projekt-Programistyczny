e76c9bdbd954a7d539e8bfc675c30d895f8ebbf7
Jakub Cieślik <jakucie936@student.polsl.pl>
2021-08-01 18:28:18 +0200

    added migration

---
+++ src/Infrastructure/Migrations/20210801162800_AddedOrderAddress.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class AddedOrderAddress : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DestinationCity",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DestinationPostCode",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DestinationStreet",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DestinationCity",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "DestinationPostCode",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "DestinationStreet",
+                table: "Orders");
+        }
+    }
+}
