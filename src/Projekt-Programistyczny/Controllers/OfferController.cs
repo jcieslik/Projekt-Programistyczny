bc7d8f56042d4c93a7ab53db64730953af6b1ade
Jakub Cieślik <jakucie936@student.polsl.pl>
2021-09-06 16:16:06 +0200

    fixes

--- src/Projekt-Programistyczny/Controllers/OfferController.cs
+++ src/Projekt-Programistyczny/Controllers/OfferController.cs
@@ -200,11 +200,11 @@ namespace Projekt_Programistyczny.Controllers
             try
             {
                 var offerDto = new UpdateOfferDTO();
-                offerDto.Id = banDto.UserId;
+                offerDto.Id = banDto.Id;
                 offerDto.State = (int?)OfferState.Banned;
                 await _offerService.UpdateOfferAsync(offerDto);
                 
-                var offer = await _offerService.GetOfferByIdAsync(banDto.UserId);
+                var offer = await _offerService.GetOfferByIdAsync(banDto.Id);
                 var messageDto = new CreateMessageDTO();
                 messageDto.Content = "Twoje ogłoszenie zostało zbanowane!<br>" + "Tytuł ogłoszenia: " + offer.Title + "<br>"
                     + "Powód blokady: " + banDto.BanInfo + "<br><br>Prosimy spróbować ponownie po ponownym zapoznaniu się z regulaminem.<br><br>Życzymy miłego dnia!";
